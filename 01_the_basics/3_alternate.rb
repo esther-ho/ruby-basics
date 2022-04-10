70ca549ebf0aea3a55fd2cb641103b9ed346450f
Esther Ho <ekyho@ucdavis.edu>
2022-04-10 15:31:11 -0700

    Add alternative answer for Exercise 3 of chapter 'The Basics'

---
+++ 01_the_basics/3_alternate.rb
@@ -0,0 +1,17 @@
+movies = {
+  :turning_red => 2022,
+  :cars => 2006,
+  :toy_story => 1995,
+  :soul => 2020,
+  :coco => 2017,
+  :up => 2009,
+  :inside_out => 2015
+}
+
+puts movies[:turning_red]
+puts movies[:cars]
+puts movies[:toy_story]
+puts movies[:soul]
+puts movies[:coco]
+puts movies[:up]
+puts movies[:inside_out]
