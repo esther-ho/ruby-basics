03e4b137706d7fc638e3a3b4175d90109027aeb8
Esther Ho <ekyho@ucdavis.edu>
2022-04-20 16:59:57 -0700

    Modify 'name.rb' to output user's full name

--- 02_variables/name.rb
+++ 02_variables/name.rb
@@ -7,3 +7,12 @@ puts "Hello, #{name}!"
 # Exercise 3
 
 10.times { puts name }
+
+# Exercise 4
+
+print 'What is your first name? '
+first_name = gets.chomp
+print 'What is your last name? '
+last_name = gets.chomp
+
+puts "Your full name is #{first_name} #{last_name}."
