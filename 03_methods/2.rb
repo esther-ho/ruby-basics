4b7c140977897551df382bde73d936f24c938be4
Esther Ho <ekyho@ucdavis.edu>
2022-04-27 17:35:18 -0700

    Add Exercise 2 from Chapter 3 - Methods

---
+++ 03_methods/2.rb
@@ -0,0 +1,10 @@
+1. x = 2 # returns 2
+
+2. puts x = 2 # prints 2, returns nil
+
+3. p name = "Joe" # prints "Joe", returns "Joe" 
+
+4. four = "four" # returns "four"
+
+5. print something = "nothing" # prints "nothing", returns nil
+
