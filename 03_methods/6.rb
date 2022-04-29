f601a6e5750d021df29d6687cc3f0b3793cf58f1
Esther Ho <ekyho@ucdavis.edu>
2022-04-29 16:10:06 -0700

    Add Exercise 6 from Chapter 3 - Methods

---
+++ 03_methods/6.rb
@@ -0,0 +1,10 @@
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product`
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>`
+
+=begin
+The method `calculate_product` is defined in line one in irb
+In line 4, `calculate_product` is called but only passed one argument instead of 2, therefore causing an error. 
+=end
+
