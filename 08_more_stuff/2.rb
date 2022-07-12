e318c4057edf7753a97fa305bf0ab4da2f216985
Esther Ho <ekyho@ucdavis.edu>
2022-07-11 17:03:09 -0700

    Modified Exercises 2, 3 and 5

--- 08_more_stuff/2.rb
+++ 08_more_stuff/2.rb
@@ -3,4 +3,5 @@ def execute(&block)
 end
 
 execute { puts "Hello from inside the execute method!" }
-# Nothing in printed to the screen as the puts statement returns as 'nil'
+# Nothing in printed to the screen as the block was not called in the method using the `.call` method
+# Instead, the method returns a Proc object
--- 08_more_stuff/5.rb
+++ 08_more_stuff/5.rb
@@ -4,5 +4,6 @@ end
 
 execute { puts "Hello from inside the execute method!" }
 
+# The method parameter 'block' is missing the '&' sign that allows a block to be passed as a parameter
 # The `execute` method called on line 5 in the 'main' context, which called the method on line 1 expecting 1 paramenter ('block') rather than a block (when using '&block')
 # Since no parameters are passed to the `execute` method (a block was passed instead), it raises an 'ArgumentError'
